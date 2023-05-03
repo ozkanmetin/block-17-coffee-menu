98099972415faa83ff346e6669ca05f1035fc46e
ozkanmetin <69031933+ozkanmetin@users.noreply.github.com>
2023-05-03 12:09:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const coffeeMenu = require("./coffee_data");
 
-// TASK 3
+// TASK 2
 const drinkNames = [];
 coffeeMenu.forEach((drink) => {
     drinkNames.push(drink.name);
 });
 console.log(drinkNames);
 
-// TASK 4
+// TASK 3
 const costUnder5 = coffeeMenu.filter(drink => drink.price<=5);
 console.log(costUnder5);
 
-// TASK 5
+// TASK 4
 const evenNumbers = coffeeMenu.filter(drink => drink.price%2===0);
 console.log(evenNumbers);
 
-// TASK 6
+// TASK 5
 const sum = coffeeMenu.reduce((sum,drink)=>{
     const updatedSum = sum + drink.price;
     return updatedSum;
 },0);
-console.log("Sum is", sum);
+console.log("The total amount is", sum);
 
-// TASK 7
+// TASK 6
 const seasonalDrinks = coffeeMenu.filter(drink => drink.seasonal);
 console.log(seasonalDrinks);
 
-// TASK 8
+// TASK 7
 function printDrinksWIB(drink,index) {
     console.log(`${drink.name} with imported beans`);
 }
